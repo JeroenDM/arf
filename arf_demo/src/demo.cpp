6b7c4fb2ec77518bc5fa923c2528cf9d48bd5230
jeroendm <jeroendemaeyer@live.be>
2018-09-25 12:58:18 +0200

    restructure demo code

--- arf_demo/src/demo.cpp
+++ arf_demo/src/demo.cpp
@@ -11,43 +11,29 @@
 
 namespace rvt = rviz_visual_tools;
 
-class Demo
+class Rviz
 {
-  
-
 public:
 moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
-  Demo()
+  Rviz()
   {
     visual_tools_.reset(new moveit_visual_tools::MoveItVisualTools("base_link","/rviz_visual_tools"));
   }
 
-  void demo1()
-  {
-    // Create pose
-    Eigen::Affine3d pose;
-    pose = Eigen::AngleAxisd(M_PI/4, Eigen::Vector3d::UnitY()); // rotate along X axis by 45 degrees
-    pose.translation() = Eigen::Vector3d( 0.1, 0.1, 0.1 ); // translate x,y,z
+  void plotPose(Eigen::Affine3d pose);
+  void clear();
+};
 
-    // Publish arrow vector of pose
-    ROS_INFO_STREAM_NAMED("test","Publishing Arrow");
-    visual_tools_->publishAxis(pose, rvt::LARGE);
-
-    // Don't forget to trigger the publisher!
-    visual_tools_->trigger();
-  }
-
-  void plotPose(Eigen::Affine3d pose)
-  {
-    visual_tools_->publishAxis(pose, rvt::LARGE);
-    visual_tools_->trigger();
-  }
-
-  void clear()
-  {
-    visual_tools_->deleteAllMarkers();
-    visual_tools_->trigger();
-  }
+class Demo1
+{
+  std::vector<std::vector<std::vector<double>>> graph_data_;
+  std::vector<TrajectoryPoint> ee_trajectory_;
+  std::vector<std::vector<double>> shortest_path_;
+public:
+  void createAndShowTrajectory(Rviz& rviz);
+  void createGraphData(Robot& robot);
+  void calculateShortestPath(Robot& robot);
+  void showShortestPath(Robot& robot, Rviz& rviz);
 };
 
 std::vector<TrajectoryPoint> createPath();
@@ -59,112 +45,13 @@ int main(int argc, char **argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    RobotMoveitWrapper robot;
-    Demo d;
-    // robot.printCurrentJointValues();
-    // d.demo1();
-    // std::vector<double> q1 = {0.1, 0.2, 0.3, 0.5, 0.6, 0.7};
-    // robot.plot(d.visual_tools_, q1);
-
-    // create a path
-    std::vector<TrajectoryPoint> demo_trajectory;
-    for (int i = 0; i < 10; ++i)
-    {
-      TolerancedNumber x(0.5, 0.45, 0.55, 5);
-      Number y, z(0.5 + static_cast<double>(i) / 20);
-      Number rx, ry(M_PI_2), rz;
-      TrajectoryPoint tp(x, y, z, rx, ry, rz);
-      demo_trajectory.push_back(tp);
-    }
-    // plot path
-    IKSolution sol;
-    for (auto tp : demo_trajectory)
-    {
-      tp.plot(d.visual_tools_);
-      // sol = robot.ik(tp.getNominalPose());
-      // for (auto q_sol : sol)
-      // {
-      //   robot.plot(d.visual_tools_, q_sol);
-      //   ros::Duration(0.1).sleep();
-      // }
-    }
-    ros::Duration(1.0).sleep();
-
-    // IKSolution sol;
-    // for (auto tp : demo_trajectory)
-    // {
-    //   sol = robot.ik(tp.getNominalPose());
-    //   for (auto q_sol : sol)
-    //   {
-    //     robot.plot(d.visual_tools_, q_sol);
-    //     ros::Duration(0.5).sleep();
-    //   }
-    // }
-
-    
-
-    std::vector<std::vector<std::vector<double>>> graph_data;
-    //IKSolution sol;
-
-    for (auto tp : demo_trajectory)
-    {
-      std::vector<std::vector<double>> new_data;
-      for (auto pose : tp.getGridSamples())
-      {
-        for (auto q_sol : robot.ik(pose))
-        {
-          new_data.push_back(q_sol);
-        }
-      }
-      graph_data.push_back(new_data);
-    }
-
-    // create Graph
-    Graph demo_graph(graph_data);
-
-    
-
-    demo_graph.runMultiSourceDijkstra();
-    std::vector<Node*> sp = demo_graph.getShortestPath();
-    std::cout << "Shortest path \n";
-    std::vector<std::vector<double>> shortest_path;
-    for (auto node : sp)
-    {
-      std::cout << (*node) << std::endl;
-      shortest_path.push_back(*(*node).jv);
-    }
-
-    for (auto q : shortest_path)
-    {
-      robot.plot(d.visual_tools_, q);
-      ros::Duration(0.5).sleep();
-    }
-
-    // std::vector<Eigen::Affine3d> discrete_poses;
-    // discrete_poses = tp.getGridSamples();
-    // for (auto p : discrete_poses)
-    // {
-    //   d.plotPose(p);
-    // }
-
-    // IKSolution sol;
-    // sol = robot.ik(tp.getNominalPose());
-    // for (auto q_sol : sol)
-    // {
-    //   if (!robot.isInCollision(q_sol))
-    //   {
-    //     robot.plot(d.visual_tools_, q_sol);
-    //     ros::Duration(0.5).sleep();
-    //   }
-    // }
-
-    ros::Duration(2.0).sleep();
-    d.clear();
-
-    // std::vector<double> q1 = {0, 0, 0, 0, 0, 0};
-    // ROS_INFO_STREAM("Robot in collision? " << robot.isInCollision(q1)); 
-    // ros::Duration(1.0).sleep();
-
+    Robot robot;
+    Rviz rviz;
+    Demo1 demo1;
+    demo1.createAndShowTrajectory(rviz);
+    demo1.createGraphData(robot);
+    demo1.calculateShortestPath(robot);
+    demo1.showShortestPath(robot, rviz);
     ros::shutdown();
     return 0;
 }
@@ -190,4 +77,71 @@ std::vector<TrajectoryPoint> createPath()
     s += ds;
   }
   return path;
+}
+
+
+void Rviz::plotPose(Eigen::Affine3d pose)
+{
+  visual_tools_->publishAxis(pose, rvt::LARGE);
+  visual_tools_->trigger();
+}
+
+void Rviz::clear()
+{
+  visual_tools_->deleteAllMarkers();
+  visual_tools_->trigger();
+}
+
+void Demo1::createAndShowTrajectory(Rviz& rviz)
+{
+  for (int i = 0; i < 10; ++i)
+  {
+    TolerancedNumber x(0.5, 0.45, 0.55, 5);
+    Number y, z(0.5 + static_cast<double>(i) / 20);
+    Number rx, ry(M_PI_2), rz;
+    TrajectoryPoint tp(x, y, z, rx, ry, rz);
+    ee_trajectory_.push_back(tp);
+  }
+  for (auto tp : ee_trajectory_)
+  {
+    tp.plot(rviz.visual_tools_);
+  }
+}
+
+void Demo1::createGraphData(Robot& robot)
+{
+  for (auto tp : ee_trajectory_)
+  {
+    std::vector<std::vector<double>> new_data;
+    for (auto pose : tp.getGridSamples())
+    {
+      for (auto q_sol : robot.ik(pose))
+      {
+        new_data.push_back(q_sol);
+      }
+    }
+    graph_data_.push_back(new_data);
+  }
+}
+
+void Demo1::calculateShortestPath(Robot& robot)
+{
+  Graph demo_graph(graph_data_);
+  demo_graph.runMultiSourceDijkstra();
+  std::vector<Node*> sp = demo_graph.getShortestPath();
+  std::cout << "Shortest path \n";
+  for (auto node : sp)
+  {
+    std::cout << (*node) << std::endl;
+    shortest_path_.push_back(*(*node).jv);
+  }
+}
+
+void Demo1::showShortestPath(Robot& robot, Rviz& rviz)
+{
+  for (auto q : shortest_path_)
+  {
+    robot.plot(rviz.visual_tools_, q);
+    ros::Duration(0.5).sleep();
+  }
 }
