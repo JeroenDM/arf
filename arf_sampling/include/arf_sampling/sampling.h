a9afe19a3c6e17578c28e30622bf5abe6b83a025
jeroendm <jeroendemaeyer@live.be>
2018-10-08 17:01:35 +0200

    remove sampled_ranges_ attribute from Sampler

--- arf_sampling/include/arf_sampling/sampling.h
+++ arf_sampling/include/arf_sampling/sampling.h
@@ -8,17 +8,17 @@ std::vector<double> range(double lower_bound, double upper_bound, int num_sample
 
 class Sampler
 {
-  int dimensions_ = 0;
-  std::vector<int> num_samples_;
-  std::vector<double> lower_bounds_;
-  std::vector<double> upper_bounds_;
-  std::vector<std::vector<double>> sampled_ranges_;
+    int dimensions_ = 0;
+    std::vector<double> lower_bounds_;
+    std::vector<double> upper_bounds_;
+    std::vector<int> num_samples_;
 
-  void recursiveGridSampling(int index, std::vector<double> prev_values, std::vector<std::vector<double>>& grid);
-public:
-  Sampler() = default;
-  void addDimension(double lower_bound, double upper_bound, int num_samples);
-  std::vector<std::vector<double>> getGridSamples();
+    void recursiveGridSampling(int index, std::vector<double> prev_values, std::vector<std::vector<double>>& grid);
+
+  public:
+    Sampler() = default;
+    void addDimension(double lower_bound, double upper_bound, int num_samples);
+    std::vector<std::vector<double>> getGridSamples();
 };
 
 #endif
--- arf_sampling/src/sampling.cpp
+++ arf_sampling/src/sampling.cpp
@@ -2,65 +2,60 @@
 
 std::vector<double> range(double lower_bound, double upper_bound, int num_samples)
 {
-  std::vector<double> range;
-  if (num_samples == 1)
-  {
-    range = {lower_bound};
-  }
-  else
-  {
-    range.resize(num_samples);
-    double increment = (upper_bound - lower_bound) / (num_samples - 1);
-    for (int i = 0;  i < num_samples; ++i)
+    std::vector<double> range;
+    if (num_samples == 1)
     {
-      range[i] = lower_bound + i * increment;
+        range = { lower_bound };
     }
-  }
-  return range;
+    else
+    {
+        range.resize(num_samples);
+        double increment = (upper_bound - lower_bound) / (num_samples - 1);
+        for (int i = 0; i < num_samples; ++i)
+        {
+            range[i] = lower_bound + i * increment;
+        }
+    }
+    return range;
+}
+
+void Sampler::recursiveGridSampling(int index, std::vector<double> prev_values, std::vector<std::vector<double>>& grid)
+{
+    auto sample_range = range(lower_bounds_[index], upper_bounds_[index], num_samples_[index]);
+    if (index < (dimensions_ - 1))
+    {
+        for (auto value : sample_range)
+        {
+            std::vector<double> pvi(prev_values);
+            pvi.push_back(value);
+            recursiveGridSampling(index + 1, pvi, grid);
+        }
+    }
+    else
+    {
+        for (auto value : sample_range)
+        {
+            std::vector<double> pvi(prev_values);
+            pvi.push_back(value);
+            std::vector<double> new_element(pvi);
+            grid.push_back(new_element);
+        }
+    }
 }
 
 void Sampler::addDimension(double lower_bound, double upper_bound, int num_samples)
 {
-  dimensions_ += 1;
-  num_samples_.push_back(num_samples);
-  lower_bounds_.push_back(lower_bound);
-  upper_bounds_.push_back(upper_bound);
-  sampled_ranges_.push_back(range(lower_bound, upper_bound, num_samples));
+    dimensions_ += 1;
+    lower_bounds_.push_back(lower_bound);
+    upper_bounds_.push_back(upper_bound);
+    num_samples_.push_back(num_samples);
 }
 
 std::vector<std::vector<double>> Sampler::getGridSamples()
 {
-  std::vector<std::vector<double>> grid;
-  std::vector<double> empty_vector;
-  recursiveGridSampling(0, empty_vector, grid);
+    std::vector<std::vector<double>> grid;
+    std::vector<double> empty_vector;
+    recursiveGridSampling(0, empty_vector, grid);
 
-  return grid;
+    return grid;
 }
-
- void Sampler::recursiveGridSampling(int index, std::vector<double> prev_values, std::vector<std::vector<double>>& grid)
-{
-  //std::cout << "Index: " << index << std::endl;
-  if (index < (dimensions_ - 1))
-  {
-    //std::cout << "recursion" << std::endl;
-    for (auto value : sampled_ranges_[index])
-    {
-      std::vector<double> pvi(prev_values);
-      pvi.push_back(value);
-      recursiveGridSampling(index + 1, pvi, grid);
-    }
-  }
-  else
-  {
-    //std::cout << "final call" << std::endl;
-    for (auto value : sampled_ranges_[index])
-    {
-      std::vector<double> pvi(prev_values);
-      pvi.push_back(value);
-      std::vector<double> new_element(pvi);
-      grid.push_back(new_element);
-    }
-  }
-  
-}
-
