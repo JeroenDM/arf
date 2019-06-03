ff0936470ff89dd4185870b58169597bf49c0ad7
JeroenDM <jeroendemaeyer@live.be>
2019-06-03 13:19:06 +0200

    new demo loaded from csv file

--- arf_demo/src/demo_table.cpp
+++ arf_demo/src/demo_table.cpp
@@ -1,28 +1,17 @@
 #include <ros/ros.h>
 #include <vector>
 #include <Eigen/Dense>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-#include <moveit_visual_tools/moveit_visual_tools.h>
-
+#include "ros/package.h"
 #include "arf_moveit_wrapper/moveit_wrapper.h"
 #include "arf_trajectory/trajectory.h"
 #include "arf_graph/graph.h"
 #include "arf_graph/util.h"
 
-namespace rvt = rviz_visual_tools;
-
-class Rviz
-{
-public:
-  moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
-  Rviz()
-  {
-    visual_tools_.reset(new moveit_visual_tools::MoveItVisualTools("base_link", "/rviz_visual_tools"));
-  }
-
-  void plotPose(Eigen::Affine3d pose);
-  void clear();
-};
+#include "util.h"
 
 class Demo1
 {
@@ -38,42 +27,67 @@ public:
   void calculateShortestPath(Robot& robot);
   void showShortestPath(Robot& robot, Rviz& rviz);
   void orientationFreeSampling(Robot& robot);
+  void orientationFreeSampling2(Robot& robot, Rviz& rviz);
+  void sampleNearSolution(Robot& robot, Rviz& rviz, double dist);
+  void showTrajectory(Rviz& rviz);
+  void readTaskFromYaml(const std::string filename);
 };
 
 std::vector<TrajectoryPoint> createPath();
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "test_moveit_wrapper");
+  ros::init(argc, argv, "demo_table");
   ros::NodeHandle node_handle;
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
+  
+
   Robot robot;
   Rviz rviz;
   Demo1 demo1;
-  demo1.createAndShowTrajectory2(rviz);
+
+  rviz.clear();
+
+  std::string filename = ros::package::getPath("arf_demo") + "/config/table_task.csv";
+
+  demo1.readTaskFromYaml(filename);
+  demo1.showTrajectory(rviz);
+
+  
+
+  // demo1.createAndShowTrajectory2(rviz);
 
   //demo1.createGraphData(robot);
   demo1.orientationFreeSampling(robot);
+  //demo1.orientationFreeSampling2(robot, rviz);
 
   demo1.calculateShortestPath(robot);
   demo1.showShortestPath(robot, rviz);
+
+  std::vector<double> home = {0, 0, 0, 0, 0, 0};
+  robot.plot(rviz.visual_tools_, home);
+
+  demo1.sampleNearSolution(robot, rviz, 0.2);
+  demo1.calculateShortestPath(robot);
+  demo1.showShortestPath(robot, rviz);
+
+  rviz.clear();
+  demo1.showTrajectory(rviz);
+
+  rviz.clear();
+
+  demo1.sampleNearSolution(robot, rviz, 0.05);
+  demo1.calculateShortestPath(robot);
+  demo1.showShortestPath(robot, rviz);
+
+  rviz.clear();
+  demo1.showTrajectory(rviz);
+
   ros::shutdown();
+
   return 0;
-}
-
-void Rviz::plotPose(Eigen::Affine3d pose)
-{
-  Eigen::Isometry3d pose_temp(pose.matrix());
-  visual_tools_->publishAxis(pose_temp, rvt::LARGE);
-  visual_tools_->trigger();
-}
-
-void Rviz::clear()
-{
-  visual_tools_->deleteAllMarkers();
-  visual_tools_->trigger();
 }
 
 void Demo1::createAndShowTrajectory(Rviz& rviz)
@@ -94,6 +108,14 @@ void Demo1::createAndShowTrajectory(Rviz& rviz)
   }
 }
 
+void Demo1::showTrajectory(Rviz& rviz)
+{
+  for (auto tp : ee_trajectory_2_)
+  {
+    tp.plot(rviz.visual_tools_);
+  }
+}
+
 void Demo1::createAndShowTrajectory2(Rviz& rviz)
 {
   for (int i = 0; i < 10; ++i)
@@ -101,12 +123,46 @@ void Demo1::createAndShowTrajectory2(Rviz& rviz)
     Number x(0.8);
     Number y(-0.2 + static_cast<double>(i) / 20);
     Number z(0.2);
-    FreeOrientationPoint tp(x, y, z, 1000);
+    Number rx(0.0), ry(M_PI), rz(0.0);
+    FreeOrientationPoint tp(x, y, z, rx, ry, rz);
     ee_trajectory_2_.push_back(tp);
   }
-  for (auto tp : ee_trajectory_)
+  for (auto tp : ee_trajectory_2_)
   {
     tp.plot(rviz.visual_tools_);
+  }
+}
+
+void Demo1::readTaskFromYaml(const std::string filename)
+{
+  ee_trajectory_2_.clear();
+
+  std::ifstream data_file;
+  data_file.open(filename);
+
+  std::string line, number;
+  std::vector<double> pt;
+  int i = -1;
+  while (std::getline(data_file, line))
+  {
+    if (i < 0)
+    {
+      // skip first line
+      ++i;
+      continue;
+    }
+    std::stringstream line_stream(line);
+    pt.clear();
+    while(std::getline(line_stream, number, ','))
+    {
+      pt.push_back(std::stod(number));
+    }
+    
+    Number x(pt[0]), y(pt[1]), z(pt[2]);
+    Number rx(pt[3]), ry(pt[4]), rz(pt[5]);
+    FreeOrientationPoint tp(x, y, z, rx, ry, rz);
+    ee_trajectory_2_.push_back(tp);
+    ++i;
   }
 }
 
@@ -152,8 +208,77 @@ void Demo1::orientationFreeSampling(Robot& robot)
   for (auto tp : ee_trajectory_2_)
   {
     std::vector<std::vector<double>> new_data;
-    for (auto pose : tp.getGridSamples())
+    for (auto pose : tp.sampleUniform(500))
     {
+      // convert tool frame to robot frame (TODO move this in robot class)
+      Eigen::Isometry3d robot_pose;
+      robot_pose = pose * tool0_to_tool_tip;
+
+      for (auto q_sol : robot.ik(robot_pose))
+      {
+        if (!robot.isInCollision(q_sol))
+          new_data.push_back(q_sol);
+      }
+    }
+    graph_data_.push_back(new_data);
+  }
+}
+
+void Demo1::sampleNearSolution(Robot& robot, Rviz& rviz, double dist)
+{
+  if (shortest_path_.size() < 1)
+  {
+    throw std::invalid_argument("There is no solution to sample around."); 
+  }
+
+  auto temp = robot.getLinkFixedRelativeTransform("torch") * robot.getLinkFixedRelativeTransform("tool_tip");
+  Transform tool0_to_tool_tip(temp.matrix());
+  tool0_to_tool_tip = tool0_to_tool_tip.inverse();
+
+  graph_data_.clear();
+  for (std::size_t i = 0; i < shortest_path_.size(); ++i)
+  {
+    auto fk_sol = robot.fk(shortest_path_[i]);
+    Transform nom_pose(fk_sol.matrix());
+    ee_trajectory_2_[i].setNominalPose(nom_pose);
+
+    std::vector<std::vector<double>> new_data;
+    for (auto pose : ee_trajectory_2_[i].sampleUniformNear(dist, 100))
+    {
+      rviz.plotPose(pose);
+
+      Eigen::Isometry3d robot_pose;
+      robot_pose = pose * tool0_to_tool_tip;
+
+      for (auto q_sol : robot.ik(robot_pose))
+      {
+        if (!robot.isInCollision(q_sol))
+          new_data.push_back(q_sol);
+      }
+    }
+    graph_data_.push_back(new_data);
+  }
+}
+
+void Demo1::orientationFreeSampling2(Robot& robot, Rviz& rviz)
+{
+  // xyz="0.315 0 0.035" rpy="0 2.3560569232 0" -1.57079632679
+  auto temp = robot.getLinkFixedRelativeTransform("torch") * robot.getLinkFixedRelativeTransform("tool_tip");
+  Transform tool0_to_tool_tip(temp.matrix());
+  tool0_to_tool_tip = tool0_to_tool_tip.inverse();
+
+  std::cout << "Trajectory length: " << ee_trajectory_2_.size() << std::endl;
+
+  for (auto tp : ee_trajectory_2_)
+  {
+    std::vector<std::vector<double>> new_data;
+
+    Eigen::Isometry3d near_pose = tp.getNominalPose();
+
+    for (auto pose :tp.sampleUniformNear(0.1, 500))
+    {
+      //pose = pose * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY());
+      rviz.plotPose(pose);
       // convert tool frame to robot frame (TODO move this in robot class)
       Eigen::Isometry3d robot_pose;
       robot_pose = pose * tool0_to_tool_tip;
@@ -174,6 +299,7 @@ void Demo1::calculateShortestPath(Robot& robot)
   demo_graph.runMultiSourceDijkstra();
   std::vector<Node*> sp = demo_graph.getShortestPath();
   std::cout << "Shortest path \n";
+  shortest_path_.clear();
   for (auto node : sp)
   {
     std::cout << (*node) << std::endl;
---
+++ arf_demo/src/util.h
@@ -0,0 +1,31 @@
+
+#include <Eigen/Dense>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+namespace rvt = rviz_visual_tools;
+
+class Rviz
+{
+public:
+  moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
+  Rviz()
+  {
+    visual_tools_.reset(new moveit_visual_tools::MoveItVisualTools("base_link", "/rviz_visual_tools"));
+  }
+
+  void plotPose(Eigen::Affine3d pose);
+  void clear();
+};
+
+void Rviz::plotPose(Eigen::Affine3d pose)
+{
+  Eigen::Isometry3d pose_temp(pose.matrix());
+  visual_tools_->publishAxis(pose_temp, rvt::LARGE);
+  visual_tools_->trigger();
+}
+
+void Rviz::clear()
+{
+  visual_tools_->deleteAllMarkers();
+  visual_tools_->trigger();
+}
