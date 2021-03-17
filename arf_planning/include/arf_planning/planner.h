4638583a105a05b66e07da7d079938f10a7c9d10
JeroenDM <jeroendemaeyer@live.be>
2021-03-17 18:37:36 +0100

    quick fix old planning code for new simple_moveit_wrapper

--- arf_planning/include/arf_planning/planner.h
+++ arf_planning/include/arf_planning/planner.h
@@ -5,35 +5,44 @@
 
 #include <ros/ros.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
+#include <simple_moveit_wrapper/robot.h>
 
-#include "arf_moveit_wrapper/redundant_robot.h"
 #include "arf_trajectory/trajectory.h"
 #include "arf_graph/graph.h"
 
 namespace arf
 {
+namespace smw = simple_moveit_wrapper;
 
-class Planner {
+class Planner
+{
   std::vector<std::vector<std::vector<double>>> graph_data_;
   std::vector<TrajectoryPoint> ee_trajectory_;
   std::vector<std::vector<double>> shortest_path_;
+
 public:
   Planner() = default;
   ~Planner() = default;
-  void setTrajectory(std::vector<TrajectoryPoint>& traj) { ee_trajectory_ = traj; }
-  bool createGraphData(RedundantRobot& robot);
-  void calculateShortestPath(RedundantRobot& robot);
-  std::vector<std::vector<double>> getShortestPath() { return shortest_path_; }
-  void showShortestPath(RedundantRobot& robot, moveit_visual_tools::MoveItVisualToolsPtr vs);
+  void setTrajectory(std::vector<TrajectoryPoint>& traj)
+  {
+    ee_trajectory_ = traj;
+  }
+  bool createGraphData(smw::Robot& robot);
+  void calculateShortestPath(smw::Robot& robot);
+  std::vector<std::vector<double>> getShortestPath()
+  {
+    return shortest_path_;
+  }
+  void showShortestPath(smw::Robot& robot, moveit_visual_tools::MoveItVisualToolsPtr vs);
 
   const std::vector<std::vector<double>>& getTPData(int index)
   {
     return graph_data_.at(index);
   }
 
-  bool run(RedundantRobot& robot, std::vector<TrajectoryPoint>& task);
+  bool run(smw::Robot& robot, std::vector<TrajectoryPoint>& task);
 };
 
-} // namespace arf
+}  // namespace arf
 
 #endif
--- arf_planning/src/planner.cpp
+++ arf_planning/src/planner.cpp
@@ -2,9 +2,17 @@
 
 namespace arf
 {
-
-bool Planner::createGraphData(RedundantRobot& robot)
+bool Planner::createGraphData(smw::Robot& robot)
 {
+  // hardcoded redundant joint samples
+  // this was implemented in the robot class in the past
+  // we could add a sampler here, but I'm not sure how much I will use this planning class
+  // TODO sample redundant joints here!
+  assert(robot.getNumRedDof() == 1);  // hard coded for a single redundant dof;
+  std::vector<std::vector<double>> q_red_samples;
+  for (double v{ 0.0 }; v <= 2.0; v += 0.1)
+    q_red_samples.push_back({ v });
+
   ROS_INFO_STREAM("Received trajectory of with points: " << ee_trajectory_.size());
   for (auto tp : ee_trajectory_)
   {
@@ -14,20 +22,24 @@ bool Planner::createGraphData(RedundantRobot& robot)
       ROS_INFO_STREAM("Fixed joint pose tp.");
       new_data.push_back(tp.joint_pose_);
     }
-    else {
+    else
+    {
       for (auto pose : tp.getGridSamples())
       {
-        for (auto q_sol : robot.ikGridSamples(pose))
+        for (auto q_red : q_red_samples)
         {
-          if (!robot.isInCollision(q_sol))
+          for (auto q_sol : robot.ik(pose, q_red))
+          {
+            if (!robot.isColliding(q_sol))
               new_data.push_back(q_sol);
+          }
         }
       }
       if (new_data.size() == 0)
       {
-          ROS_ERROR("No collision free ik sol found for a tp");
-          //throw std::runtime_error("No ik found");
-          return false;
+        ROS_ERROR("No collision free ik sol found for a tp");
+        // throw std::runtime_error("No ik found");
+        return false;
       }
       ROS_INFO_STREAM("Found collision free solutions: " << new_data.size());
     }
@@ -36,7 +48,7 @@ bool Planner::createGraphData(RedundantRobot& robot)
   return true;
 }
 
-void Planner::calculateShortestPath(RedundantRobot& robot)
+void Planner::calculateShortestPath(smw::Robot& robot)
 {
   Graph demo_graph(graph_data_);
   demo_graph.runMultiSourceDijkstra();
@@ -44,12 +56,12 @@ void Planner::calculateShortestPath(RedundantRobot& robot)
   std::cout << "Shortest path \n";
   for (auto node : sp)
   {
-    //std::cout << (*node) << std::endl;
+    // std::cout << (*node) << std::endl;
     shortest_path_.push_back(*(*node).jv);
   }
 }
 
-void Planner::showShortestPath(RedundantRobot& robot, moveit_visual_tools::MoveItVisualToolsPtr vs)
+void Planner::showShortestPath(smw::Robot& robot, moveit_visual_tools::MoveItVisualToolsPtr vs)
 {
   for (auto q : shortest_path_)
   {
@@ -58,7 +70,7 @@ void Planner::showShortestPath(RedundantRobot& robot, moveit_visual_tools::MoveI
   }
 }
 
-bool Planner::run(RedundantRobot& robot, std::vector<TrajectoryPoint>& task)
+bool Planner::run(smw::Robot& robot, std::vector<TrajectoryPoint>& task)
 {
   setTrajectory(task);
   if (createGraphData(robot))
@@ -69,4 +81,4 @@ bool Planner::run(RedundantRobot& robot, std::vector<TrajectoryPoint>& task)
   return false;
 }
 
-} // namespace arf
+}  // namespace arf
